88cdc4b6cd03b8cb08bcc264cfaeddc82bcf4902
Terje Sandstrom <terje@osiris.no>
2013-09-16 23:52:03 +0200

    removed unused using

--- Osiris TFS Monitor/ViewModels/SlideElements/BuildMonitor/BuildMonitorVm.cs
+++ Osiris TFS Monitor/ViewModels/SlideElements/BuildMonitor/BuildMonitorVm.cs
@@ -12,19 +12,13 @@ namespace Osiris.Tfs.Monitor
 
 	using System;
 	using System.Linq;
-	using System.Text;
-	using System.Diagnostics;
-	using System.Windows.Input;
-	using Osiris.Tfs.Monitor.Models;
-	using Osiris.Tfs.Report;
-	using Microsoft.TeamFoundation.VersionControl.Client;
-	using System.Net;
-	using Microsoft.TeamFoundation.Client;
-	using Osiris.Tfs.Monitor.Properties;
+    using System.Diagnostics;
+    using Osiris.Tfs.Monitor.Models;
+    using System.Net;
+    using Osiris.Tfs.Monitor.Properties;
 	using System.IO;
 	using System.Collections.ObjectModel;
-	using Microsoft.Practices.Composite.Events;
-	using Osiris.TFS.Monitor.Common;
+    using Osiris.TFS.Monitor.Common;
 	using Microsoft.TeamFoundation.Build.Client;
 	using Microsoft.Practices.Composite.Presentation.Events;
 
