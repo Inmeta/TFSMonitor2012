96909735ed30278ad9b5bea0ff69c2d9d957726d
Tor Arne <torarne.vinslid@inmeta.no>
2014-01-03 13:00:14 +0100

    Updated assembley version info

--- Osiris TFS Monitor/Properties/AssemblyInfo.cs
+++ Osiris TFS Monitor/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.5.200")]
-[assembly: AssemblyFileVersion("2.5.5.200")]
+[assembly: AssemblyVersion("2.5.6.200")]
+[assembly: AssemblyFileVersion("2.5.6.200")]
